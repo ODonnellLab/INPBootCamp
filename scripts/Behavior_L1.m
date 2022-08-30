2f1c892c1f40c91394064e7615c43ae288e701ae
jshomar <53152427+jshomar@users.noreply.github.com>
2022-08-30 00:31:18 -0400

    Update Behavior_L1.m

--- scripts/Behavior_L1.m
+++ scripts/Behavior_L1.m
@@ -1,6 +1,6 @@
 % Read in data
 % Change this filePath to load different experiments
-filePath = '/datasets/1D/WT/HEX/20200623_N2_HEX_10000/all_matTrack_data.csv';
+filePath = '../datasets/1D/WT/HEX/20200623_N2_HEX_10000/all_matTrack_data.csv';
 ASSAY = readtable(filePath);
 
 % Before doing anything with the data, it is typically helpful to
